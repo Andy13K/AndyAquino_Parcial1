d4859ce5588aa75dd7a2eddfd52e22e816547ce8
Andy13K <aaquinoe1@miumg.edu.gt>
2024-03-02 12:50:04 -0600

    Subiendo segundo ejercicio de primer parcial PrograIII

--- AndyAquino_Parcial1.php
+++ AndyAquino_Parcial1.php
@@ -18,4 +18,19 @@ for ($i = 0; $i <= 50; $i++) {
 echo "La suma de los numeros impares de 0 a 50 es: $a \n";
 echo "\nLa suma de los numeros pares de 0 a 50 es: $b \n";
 
+echo"\n---------------------------------------------------------------------------------\n";
+
+$num = 0;
+
+while ($num <= 30 || $num % 2 == 0) {
+    if ($num > 0) {
+        echo "$num, ";
+    }
+    $num += 3;
+}
+
+echo "\nEl primer numero impar despues de 30 de 3 en 3 es: $num";
+
+//https://onlinephp.io/c/b5df3
+
 ?>
